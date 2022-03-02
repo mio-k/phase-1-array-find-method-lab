681ccd066a79ea98b61d5acd233c721b2d7add09
mio kataoka <mio.kanna@gmail.com>
2022-03-02 11:35:42 -0800

    commit tree 764c6b91e255005da40ec72ad867b4697c9cd4aa

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record){
+    const won = record.find(record => record.result === `W`);
+    if (won){
+        return won.year;
+  } else {
+      return undefined
+  }
+}
